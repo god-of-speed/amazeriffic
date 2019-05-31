c439a13dd3739985e8e512c82fe39d6c53bdfa94
Ebuka Ubah <ebukaubah78@gmail.com>
2019-05-31 13:25:54 +0100

    cleaned

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ var app = express();
 app.use(express.static('./public/assets'));
 app.set('view engine','ejs');
 controller(app);
-app.listen(3000);
+app.listen(process.env.PORT || 3000, function() {});
 console.log('connected on log 3000');
